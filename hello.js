63838cabd62fab54920c83388800b82cd674d8fa
takayuki-s <taka12yuki04@gmail.com>
2021-04-09 17:44:16 +0900

    blockingとnon blockingの書き方を自作で比較

--- hello.js
+++ hello.js
@@ -1,10 +1,38 @@
 // non blocking
-setTimeout(function() {
-  console.log("hello");
-}, 1000);
-console.log("world");
+// setTimeout(function() {
+//   console.log("hello");
+// }, 1000);
+// console.log("world");
 
 // blocking
-var start = new Date().getTime();
-while (new Date().getTime() < start + 1000);
-console.log("world");
+// var start = new Date().getTime();
+// while (new Date().getTime() < start + 1000);
+// console.log("world");
+
+// blocking
+var x = 0;
+while (x<3) {
+  x++;
+  console.log(x)
+}
+console.log("finish!");
+
+// non blocking
+var y = 3;
+setTimeout(function() {
+  while (y<6) {
+  y++;
+  console.log(y)
+  }
+});
+console.log("finish!");
+
+// non blocking
+var z = 6;
+const zSet = function () {
+  while (z<9) {
+  z++;
+  console.log(z)
+  }
+};
+console.log("finish!");
