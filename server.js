b6fb3fda10befbfda31f0ed702ecb120c8592da2
takayuki-s <taka12yuki04@gmail.com>
2021-04-10 09:10:31 +0900

    外部ファイルにサーバーを設定して変数化

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var http = require('http');
+var settings = require('./settings.js') 
 var server = http.createServer();
 server.on('request', function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('hello world !!!');
     res.end();
 });
-server.listen(3000);
+server.listen(settings.port);
 console.log("server listening ...");
