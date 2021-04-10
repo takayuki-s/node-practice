8914869d000144258fbbf3728a863387e5f30187
takayuki-s <taka12yuki04@gmail.com>
2021-04-10 09:29:06 +0900

    htmlファイル読み込み

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 var http = require('http');
+  fs = require('fs');
 var settings = require('./settings.js') 
 var server = http.createServer();
+var msg;
 server.on('request', function(req, res) {
-    switch(req.url) {
-      case '/about' :
-        msg = "about this page";
-        break;
-      case '/profile' :
-        msg = "about me";
-        break;
-      default:
-        msg = "wrong page";
-        break;
+    fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.write("not found!");
+      return res.end();
     }
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(msg);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
     res.end();
+    });
 });
 server.listen(settings.port);
 console.log("server listening ...");
