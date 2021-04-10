5817074711b893248d73998a3d31b390b118571b
takayuki-s <taka12yuki04@gmail.com>
2021-04-10 09:15:58 +0900

    リクエストURLによる処理の振り分け

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ var http = require('http');
 var settings = require('./settings.js') 
 var server = http.createServer();
 server.on('request', function(req, res) {
+    switch(req.url) {
+      case '/about' :
+        msg = "about this page";
+        break;
+      case '/profile' :
+        msg = "about me";
+        break;
+      default:
+        msg = "wrong page";
+        break;
+    }
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('hello world !!!');
+    res.write(msg);
     res.end();
 });
 server.listen(settings.port);
