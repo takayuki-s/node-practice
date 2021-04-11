b4af134baf05648ac73b4264df8abfcd73b6f229
takayuki-s <taka12yuki04@gmail.com>
2021-04-11 10:41:20 +0900

    エスケープ処理で統一して表示

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ server.on('request', function(req, res) {
     n++;
     var data = ejs.render(template, {
         title: "hello",
-        content: "<strong>World!</strong>",
+        content: "World!",
         n: n
     });
     res.writeHead(200, {'Content-Type': 'text/html'});
