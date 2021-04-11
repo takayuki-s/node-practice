04184a4634eda718a22d94a0e767d488b0c35f0d
takayuki-s <taka12yuki04@gmail.com>
2021-04-11 11:38:05 +0900

    投稿データを表示

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
-var http = require('http');
-  fs = require('fs');
-  ejs = require('ejs');
+var http = require('http'),
+  fs = require('fs'),
+  ejs = require('ejs'),
+  qs = require('querystring');
 var settings = require('./settings.js') 
 var server = http.createServer();
-var template = fs.readFileSync(__dirname + '/public_html/hello.ejs', 'utf-8');
-var n = 0
+var template = fs.readFileSync(__dirname + '/public_html/bbs.ejs', 'utf-8');
+var posts = [];
+function renderForm(posts, res) {
+  var data = ejs.render(template, {
+    posts: posts
+  });
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(data);
+  res.end();
+}
 server.on('request', function(req, res) {
-    n++;
-    var data = ejs.render(template, {
-        title: "hello",
-        content: "World!",
-        n: n
-    });
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
+    if (req.method === 'POST') {
+      req.data = "";
+      req.on("data", function(chunk) {
+        req.data += chunk;
+      });
+      req.on("end", function() {
+        var query = qs.parse(req.data);
+        posts.push(query.name);
+        renderForm(posts, res);
+      });
+    } else {
+      renderForm(posts, res);
+    }
 });
 server.listen(settings.port);
 console.log("server listening ...");
