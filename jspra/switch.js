ad74b30fe9490b01184c340fbe4045a1a394aead
takayuki-s <taka12yuki04@gmail.com>
2021-04-17 09:17:36 +0900

    switch文練習

---
+++ jspra/switch.js
@@ -0,0 +1,19 @@
+const rank = 2;
+
+switch (rank) {
+  case 1:
+    console.log("ナンバー1です");
+  break;
+
+  case 2:
+    console.log("ナンバー2です");
+  break;
+
+  case 3:
+    console.log("ナンバー3です")
+  break;
+
+  default:
+    console.log("ランキング圏外です")
+  break;
+}
