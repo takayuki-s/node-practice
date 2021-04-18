000a0be76b7361891e82d8744da81b868659c966
takayuki-s <taka12yuki04@gmail.com>
2021-04-18 12:00:43 +0900

    戻り値を応用して関数を作る

--- jspra/func.js
+++ jspra/func.js
@@ -57,3 +57,23 @@ if (check(123)) {
 } else {
   console.log("3の倍数ではありません");
 }
+
+
+//戻り値練習問題
+const number1 = 103;
+const number2 = 72;
+const number3 = 189;
+
+const getMax = (a, b, c) => {
+  if (a > b && a > c) {
+    return a;
+  } else if (b > a && b > c) {
+    return b;
+  } else {
+    return c;
+  }
+} 
+
+const max = getMax(number1, number2, number3);
+
+console.log(`最大値は${max}です`);
