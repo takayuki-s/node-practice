3b0322325f90534f62055936871cf547f7548620
takayuki-s <taka12yuki04@gmail.com>
2021-04-18 11:38:44 +0900

    引数

--- jspra/func.js
+++ jspra/func.js
@@ -1,7 +1,7 @@
 // 定数helloに関数を代入
 const hello = function() {
   console.log("こんにちは！");
-  console.log("私の名前はTEKEです");
+  console.log("私の名前はTekeTECHです");
 }
 
 // 定数helloに代入された関数を呼び出し
@@ -14,3 +14,12 @@ const greet = () => {
 
 // 定数greetを呼び出し
 greet();
+
+
+//引数
+const greeting = (name) => {
+  console.log(`こんにちは、${name}さん`);
+  
+};
+
+greeting("TekeTECH");
