b3678c0b32d28158725f99b1b2461415b776e252
takayuki-s <taka12yuki04@gmail.com>
2021-04-21 13:20:04 +0900

    初期化メソッドthis使用

---
+++ jspra/class2.js
@@ -0,0 +1,12 @@
+class Animal {
+  constructor() {
+    this.name = "レオ";
+    this.age = 3;
+  }
+}
+
+const animal = new Animal();
+
+console.log(`名前: ${animal.name}`);
+
+console.log(`年齢: ${animal.age}`);
