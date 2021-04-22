01a2cdc91c9daa588ab57176fbdb5f6bdcbb59bf
takayuki-s <taka12yuki04@gmail.com>
2021-04-22 23:40:39 +0900

    子クラス継承成功

--- jspra-module/guild.js
+++ jspra-module/guild.js
@@ -15,5 +15,28 @@ class Guild {
   }
 }
 
+class Fairytail extends Guild {
+  constructor(name, age, tech) {
+    super(name, age);
+    this.tech = tech;
+  }
+
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です！`);
+    console.log(`${this.age}歳です！`);
+    console.log(`得意技は${this.tech}です！`);
+  }
+}
+
+class Test {
+}
+
+const member = new  Fairytail ("グレイ", 18, "アイスメイク");
+member.info();
+
 // export default Guild;　←エラーになった
-module.exports = Guild;
+module.exports = {
+  Guild,
+  Fairytail
+};
--- jspra-module/script.js
+++ jspra-module/script.js
@@ -1,6 +1,10 @@
 // import Guild from "./guild";　←エラーになった
-const Guild = require("./guild.js");
+const Guild = require("./guild.js").Guild;
+const Fairytail = require("./guild.js").Fairytail;
+const Test = require("./guild.js").Test;
 
-const member = new Guild ("グレイ", 18);
+const member = new  Guild ("グレイ", 18);
 member.info();
+const member2 = new Fairytail("グレイ", 18, "アイスメイク");
+member2.info();
 
