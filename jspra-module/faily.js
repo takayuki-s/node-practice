2932dce5377dfb3aebc0477bc63d96baddec10b7
takayuki-s <taka12yuki04@gmail.com>
2021-04-22 15:35:38 +0900

    export&クラス継承（クラス定義が上手く行かないパターン）

---
+++ jspra-module/faily.js
@@ -0,0 +1,22 @@
+const Guild = require("./guild.js");
+// import Guild from "./guild"
+
+class Fairy extends Guild {
+  constructor(name, age, tech) {
+    super(name, age);
+    this.tech = tech;
+  }
+
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です！`);
+    console.log(`${this.age}歳です！`);
+    console.log(`得意技は${this.tech}です！`);
+  }
+}
+
+const member = new Guild ("グレイ", 18, "アイスメイク");
+member.info();
+
+
+// module.exports = Failytail;
--- jspra-module/script.js
+++ jspra-module/script.js
@@ -1,5 +1,5 @@
 // import Guild from "./guild";　←エラーになった
-const Guild = require("./guild");
+const Guild = require("./guild.js");
 
 const member = new Guild ("グレイ", 18);
 member.info();
